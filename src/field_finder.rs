27688fe6eb8c47610a1f34a945d50c2c0ddccaf2
vdoubleu <victorwang2001@gmail.com>
2022-06-23 18:54:44 +0000

    remove toml_document

--- src/field_finder.rs
+++ src/field_finder.rs
@@ -93,116 +93,116 @@ pub fn get_table_mut_internal<'a>(
     descend(doc.as_item_mut(), table_path, insert_if_not_exists)
 }
 
-pub fn get_field<'a>(fields_path: Vec<&str>, doc: &'a mut Document) -> Result<&'a mut Item, Error> {
-    // the field path is seperated by '/' 
-    // we need to iterate through each field in the doc
-    // we need to return the last field in the path
+// pub fn get_field<'a>(fields_path: Vec<&str>, doc: &'a mut Document) -> Result<&'a mut Item, Error> {
+//     // the field path is seperated by '/' 
+//     // we need to iterate through each field in the doc
+//     // we need to return the last field in the path
 
-    let mut fields = fields_path.clone();
+//     let mut fields = fields_path.clone();
 
-    println!("fields: {:?}", fields.len());
+//     println!("fields: {:?}", fields.len());
 
-    if fields.len() < 1 {
-        return Err(Error::new(ErrorKind::Other, "Field path is empty in get_field"));
-    }
+//     if fields.len() < 1 {
+//         return Err(Error::new(ErrorKind::Other, "Field path is empty in get_field"));
+//     }
 
-    // we need to get first field to get started
-    // we can directly grab like this since top level toml
-    // is never an array and always a table
-    let start_field_res = doc.get_mut(fields.remove(0));
-    if start_field_res.is_none() {
-        return Err(Error::new(
-            ErrorKind::Other,
-            "error: could not find field",
-        ));
-    }
-    let mut current_item = start_field_res.unwrap();
+//     // we need to get first field to get started
+//     // we can directly grab like this since top level toml
+//     // is never an array and always a table
+//     let start_field_res = doc.get_mut(fields.remove(0));
+//     if start_field_res.is_none() {
+//         return Err(Error::new(
+//             ErrorKind::Other,
+//             "error: could not find field",
+//         ));
+//     }
+//     let mut current_item = start_field_res.unwrap();
 
-    for field in fields {
-        // after the first level, we need to check if the current item is
-        // an array or a table
-        match current_item {
-            Item::Table(table) => {
-                current_item = match table.get_mut(field) {
-                    Some(item) => item,
-                    None => return Err(Error::new(
-                        ErrorKind::Other,
-                        "error: could not find field",
-                    )),
-                };
-            },
-            Item::Value(val) => {
-                match val {
-                    Value::Array(array) => {
-                        let array_index = match field.parse::<usize>() {
-                            Ok(index) => index,
-                            Err(_) => return Err(Error::new(ErrorKind::Other, "could not parse array index")),
-                        };
+//     for field in fields {
+//         // after the first level, we need to check if the current item is
+//         // an array or a table
+//         match current_item {
+//             Item::Table(table) => {
+//                 current_item = match table.get_mut(field) {
+//                     Some(item) => item,
+//                     None => return Err(Error::new(
+//                         ErrorKind::Other,
+//                         "error: could not find field",
+//                     )),
+//                 };
+//             },
+//             Item::Value(val) => {
+//                 match val {
+//                     Value::Array(array) => {
+//                         let array_index = match field.parse::<usize>() {
+//                             Ok(index) => index,
+//                             Err(_) => return Err(Error::new(ErrorKind::Other, "could not parse array index")),
+//                         };
 
-                        if array_index >= array.len() {
-                            return Err(Error::new(
-                                ErrorKind::Other,
-                                "error: array index out of bounds",
-                            ));
-                        }
+//                         if array_index >= array.len() {
+//                             return Err(Error::new(
+//                                 ErrorKind::Other,
+//                                 "error: array index out of bounds",
+//                             ));
+//                         }
 
                         
-                        let array_item: &mut Value = match array.get_mut(array_index) {
-                            Some(item) => item,
-                            None => return Err(Error::new(
-                                ErrorKind::Other,
-                                "error: array index out of bounds",
-                            )),
-                        };
+//                         let array_item: &mut Value = match array.get_mut(array_index) {
+//                             Some(item) => item,
+//                             None => return Err(Error::new(
+//                                 ErrorKind::Other,
+//                                 "error: array index out of bounds",
+//                             )),
+//                         };
 
-                        current_item = &mut Item::Value(array_item);
-                    },
-                    Value::InlineTable(table) => {
-                        current_item = match table.get_mut(field) {
-                            Some(item) => item,
-                            None => return Err(Error::new(
-                                ErrorKind::Other,
-                                "error: could not find field",
-                            )),
-                        };
-                    },
-                    _ => {
-                        return Err(Error::new(
-                            ErrorKind::Other,
-                            "error: field is not recursable",
-                        ));
-                    }
-                }
-            },
-            Item::ArrayOfTables(array) => {
-                let array_index = match field.parse::<usize>() {
-                    Ok(index) => index,
-                    Err(_) => return Err(Error::new(ErrorKind::Other, "could not parse array index")),
-                };
+//                         current_item = &mut Item::Value(array_item);
+//                     },
+//                     Value::InlineTable(table) => {
+//                         current_item = match table.get_mut(field) {
+//                             Some(item) => item,
+//                             None => return Err(Error::new(
+//                                 ErrorKind::Other,
+//                                 "error: could not find field",
+//                             )),
+//                         };
+//                     },
+//                     _ => {
+//                         return Err(Error::new(
+//                             ErrorKind::Other,
+//                             "error: field is not recursable",
+//                         ));
+//                     }
+//                 }
+//             },
+//             Item::ArrayOfTables(array) => {
+//                 let array_index = match field.parse::<usize>() {
+//                     Ok(index) => index,
+//                     Err(_) => return Err(Error::new(ErrorKind::Other, "could not parse array index")),
+//                 };
 
-                if array_index >= array.len() {
-                    return Err(Error::new(
-                        ErrorKind::Other,
-                        "error: array index out of bounds",
-                    ));
-                }
+//                 if array_index >= array.len() {
+//                     return Err(Error::new(
+//                         ErrorKind::Other,
+//                         "error: array index out of bounds",
+//                     ));
+//                 }
 
-                current_item = match array.get_mut(array_index) {
-                    Some(t) => &mut Item::Table(t),
-                    None => return Err(Error::new(
-                        ErrorKind::Other,
-                        "error: could not find field",
-                    )),
-                };
-            },
-            _ => {
-                return Err(Error::new(ErrorKind::Other, "Field path is invalid"));
-            }
-        }
-    }
+//                 current_item = match array.get_mut(array_index) {
+//                     Some(t) => &mut Item::Table(t),
+//                     None => return Err(Error::new(
+//                         ErrorKind::Other,
+//                         "error: could not find field",
+//                     )),
+//                 };
+//             },
+//             _ => {
+//                 return Err(Error::new(ErrorKind::Other, "Field path is invalid"));
+//             }
+//         }
+//     }
 
-    Ok(current_item)
-}
+//     Ok(current_item)
+// }
 
 // fn recurse_on_value(value: &mut Value) {
 //     match value {
