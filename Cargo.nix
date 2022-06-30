f4a107f92d94509506135fb63bfab4c74fc30c86
vdoubleu <victorwang2001@gmail.com>
2022-06-30 23:07:25 +0000

    updated toml-editor version

--- Cargo.nix
+++ Cargo.nix
@@ -451,7 +451,7 @@ rec {
       };
       "toml-editor" = rec {
         crateName = "toml-editor";
-        version = "0.4.0";
+        version = "0.4.1";
         edition = "2018";
         crateBin = [
           { name = "toml-editor"; path = "src/main.rs"; }
